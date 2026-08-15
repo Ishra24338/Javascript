6fabe2cc1eb50f84f955b4a563293da2b2b11a13
Ishra24338 <nadiraema48829@gmail.com>
2026-08-15 11:44:58 +0600

    Add files via upload

---
+++ my zoo.js
@@ -0,0 +1,6 @@
+document.getElementById("zone1").innerHTML ="Lion";
+document.getElementById("zone2").innerHTML ="Elephant";
+document.getElementById("zone3").innerHTML ="Penguin";
+document.getElementById("total").innerHTML ="3";
+document.getElementById("zoo").style.backgroundColor ="lightgreen";
+document.getElementById("zoo-status").innerHTML = document.title+"is now open";
